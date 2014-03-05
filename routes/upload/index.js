eade978242da890aaacf83de6335c9a9eecccf88
Olli Koistinen <olko@cs.helsinki.fi>
2014-03-05 06:45:40 +0200

    fixed uploading.. for now

--- routes/upload/index.js
+++ routes/upload/index.js
@@ -7,11 +7,14 @@ var handleUpload = function(req, res) {
   var sgf = require('../../lib/sgf.js');
   var util = require('util');
   req.form.on('part', function(part) {
+    var data = "";
     part.on('data', function(chunk) {
-      res.json(sgf.toJSON(chunk));
+      data += chunk;
+    });
+    part.on('end', function() {
+      res.json(sgf.toJSON(data));
     });
   });
-  console.log(util.inspect(req.form));
 }
 
 var renderForm = function(req, res) {
